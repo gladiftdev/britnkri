6dee784dc2955a0d346b63ae69bef35bb758fda9
gladiftdev <gladifts2@gmail.com>
2026-09-25 23:20:21 +0000

    3.0: add frontend Vite configuration

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TariqCarRentalApp from './tariq-car-rental-app'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TariqCarRentalApp />
+  </React.StrictMode>,
+)
